118584b5cc74e54bfd1dc87bb4daac2211f855d9
FalsePattern <me@falsepattern.com>
2025-07-06 02:59:36 +0200

    update gradle, buildscript, and deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("fpgradle-minecraft") version ("0.10.0")
+    id("com.falsepattern.fpgradle-mc") version ("0.19.3")
 }
 
 group = "mega"
@@ -28,13 +28,13 @@ minecraft_fp {
 }
 
 repositories {
-    mega()
+    exclusive(mega(), "mega", "codechicken")
 }
 
 dependencies {
-    implementation("it.unimi.dsi:fastutil:8.5.13")
-    runtimeOnlyNonPublishable("codechicken:notenoughitems-mc1.7.10:2.3.1-mega:dev")
-    runtimeOnlyNonPublishable("codechicken:codechickencore-mc1.7.10:1.4.0-mega:dev")
+    implementation("it.unimi.dsi:fastutil:8.5.16")
+    runtimeOnlyNonPublishable("codechicken:notenoughitems-mc1.7.10:2.7.51-mega:dev")
+    runtimeOnlyNonPublishable("codechicken:codechickencore-mc1.7.10:1.4.2-mega:dev")
 
-    devOnlyNonPublishable("mega:carpentersblocks-mc1.7.10:3.4.1-mega:dev")
+    devOnlyNonPublishable("mega:carpentersblocks-mc1.7.10:3.7.0-mega:dev")
 }
